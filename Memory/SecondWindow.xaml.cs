7a92b39a06c161a44455d0efddcf9683025abd35
George <jerzytuszynski012@gmail.com>
2019-02-16 15:38:42 +0100

    Added dispatcher and Thread and functionality finally works as expected

--- Memory/SecondWindow.xaml.cs
+++ Memory/SecondWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,7 +22,8 @@ namespace Memory
     public partial class SecondWindow : Window
     {
         string CardSource { get; } = @"C:\Users\George\source\repos\Memory\Memory\Resources\card.jpg";
-        string[] ducks =
+
+        readonly string[] ducks =
         {
             @"C:\Users\George\source\repos\Memory\Memory\Resources\duck1.jpg",
             @"C:\Users\George\source\repos\Memory\Memory\Resources\duck1.jpg",
@@ -42,8 +44,9 @@ namespace Memory
         };
         string[] randomDucks = new string[16];
         bool[] wasDuck = new bool[16];
-        Image lastImage;
+        bool[] correctlyReversedDuck = new bool[16];
         int numberOfImages = 0;
+        private Image lastImage;
 
         public SecondWindow()
         {
@@ -51,37 +54,47 @@ namespace Memory
             GenerateRandomDuckArray();
         }
 
-        
+
 
         private void Image_MouseDown(object sender, MouseButtonEventArgs e)
         {
             
-            var name = ((Image)sender).Name;
-            var stringNumber = Regex.Match(name, @"\d+").Value;
-            var number = Int32.Parse(stringNumber);
-
-            ((Image)sender).Source = new BitmapImage(
-                                        new Uri(randomDucks[number - 1]));
-
-            numberOfImages++;
-            if (numberOfImages == 1) lastImage = (Image)sender;
-            if (numberOfImages == 2)
+            var number = getNumberFromName((Image)sender);
+            if (!correctlyReversedDuck[number - 1])
             {
-                numberOfImages = 0;
-                var comparison = AreImagesTheSame((Image)sender, lastImage);
-                if(comparison)
-                {
+                ((Image)sender).Source = new BitmapImage(
+                                            new Uri(randomDucks[number - 1]));
 
-                }
-                else
+                numberOfImages++;
+                if (numberOfImages == 1) lastImage = (Image)sender;
+                if (numberOfImages == 2)
                 {
-                    ImageSource source = new BitmapImage(new Uri(CardSource));
-                    ((Image)sender).Source = source;
-                    lastImage.Source = source;
+                    numberOfImages = 0;
+                    var comparison = AreImagesTheSame((Image)sender, lastImage);
+                    if (comparison)
+                    {
+
+                    }
+                    else
+                    {
+                        Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Input, new ThreadStart(() =>
+                        {
+                            Thread.Sleep(800);
+                            ChangeIncorrectImages(sender);
+                        }
+                        ));
+                    }
                 }
             }
 
 
+        }
+
+        private void ChangeIncorrectImages(object sender)
+        {
+            ImageSource source = new BitmapImage(new Uri(CardSource));
+            ((Image)sender).Source = source;
+            lastImage.Source = source;
         }
 
         private int GetNotExistingDuck()
@@ -119,7 +132,12 @@ namespace Memory
             var number = getNumberFromName(image1);
             var number2 = getNumberFromName(image2);
 
-            if (randomDucks[number - 1].Equals(randomDucks[number2 - 1])) return true;
+            if (randomDucks[number - 1].Equals(randomDucks[number2 - 1]))
+            {
+                correctlyReversedDuck[number - 1] = true;
+                correctlyReversedDuck[number2 - 1] = true;
+                return true;
+            }
             else return false;
         }
 
